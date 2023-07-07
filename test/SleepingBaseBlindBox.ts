74e049fd1cd3789f7605fa673672d3b338f429bf
LongYan <2944972991@qq.com>
2023-07-07 15:22:24 +0800

    SleepingBaseBlindBox.ts over

--- test/SleepingBaseBlindBox.ts
+++ test/SleepingBaseBlindBox.ts
@@ -177,7 +177,100 @@ describe("SleepingBase", function () {
             });
         });
 
-        // describe("用户操作", function () {
-        // });
+        describe("用户操作", function () {
+            const generateMerkleData = readGenerateMerkleData();
+
+            it("因该可以进行正确的领取操作", async function () {
+                const {sleepingBaseBlindBox, owner} = await loadFixture(deploySleepingBaseBlindBox);
+
+                let userMerkleData = generateMerkleData.claims[owner.address];
+
+                await expect(sleepingBaseBlindBox.claim((userMerkleData.index + 1), (userMerkleData.amount + 10), userMerkleData.proof)).to.be.reverted;
+                await expect(sleepingBaseBlindBox.claim(userMerkleData.index, userMerkleData.amount, userMerkleData.proof)).not.to.be.reverted;
+            });
+
+            it("未到时间应该不可以操作", async function () {
+                const {
+                    sleepingBaseBlindBox,
+                    owner,
+                    otherAccount,
+                    sleepingBase
+                } = await loadFixture(deploySleepingBaseBlindBox);
+
+                // 为盲盒合约增加mint权限
+                let MINTER_ROLE = await sleepingBase.MINTER_ROLE()
+
+                await sleepingBase.grantRole(MINTER_ROLE, sleepingBaseBlindBox.address)
+
+                // 设置新的时间
+                let time = Math.floor(Date.now() / 1000)
+                await sleepingBaseBlindBox.setOpenAndSalesTime(timeGeneration([(time + 100).toString(), (time + 100).toString()]));
+
+                // 领取盲盒
+                let userMerkleData = generateMerkleData.claims[owner.address];
+                await sleepingBaseBlindBox.claim(userMerkleData.index, userMerkleData.amount, userMerkleData.proof);
+
+                // 批转盲盒的转移
+                await sleepingBaseBlindBox.setApprovalForAll(otherAccount.address, true);
+
+                await expect(
+                    sleepingBaseBlindBox.openBox(
+                        2,
+                        [tokenIdGeneration(["3", "4", "23", "45", "34"]), tokenIdGeneration(["4", "3", "32", "45", "33"])],
+                        ["test", "test"])
+                ).to.be.reverted;
+
+                await expect(
+                    sleepingBaseBlindBox.safeTransferFrom(
+                        owner.address,
+                        otherAccount.address,
+                        0,
+                        1,
+                        "0x")
+                ).to.be.reverted;
+            });
+
+            it("到时间应该可以操作", async function () {
+                const {
+                    sleepingBaseBlindBox,
+                    owner,
+                    otherAccount,
+                    sleepingBase
+                } = await loadFixture(deploySleepingBaseBlindBox);
+
+                // 为盲盒合约增加mint权限
+                let MINTER_ROLE = await sleepingBase.MINTER_ROLE()
+
+                await sleepingBase.grantRole(MINTER_ROLE, sleepingBaseBlindBox.address)
+
+                // 设置新的时间
+                let time = Math.floor(Date.now() / 1000)
+                await sleepingBaseBlindBox.setOpenAndSalesTime(timeGeneration([(time - 100).toString(), (time - 100).toString()]));
+
+                // 领取盲盒
+                let userMerkleData = generateMerkleData.claims[owner.address];
+                await sleepingBaseBlindBox.claim(userMerkleData.index, userMerkleData.amount, userMerkleData.proof);
+
+                // 批转盲盒的转移
+                await sleepingBaseBlindBox.setApprovalForAll(otherAccount.address, true);
+
+                await expect(
+                    sleepingBaseBlindBox.openBox(
+                        2,
+                        [tokenIdGeneration(["3", "4", "23", "45", "34"]), tokenIdGeneration(["4", "3", "32", "45", "33"])],
+                        ["test", "test"])
+                ).not.to.be.reverted;
+
+                await expect(
+                    sleepingBaseBlindBox.safeTransferFrom(
+                        owner.address,
+                        otherAccount.address,
+                        0,
+                        1,
+                        "0x")
+                ).not.to.be.reverted;
+            });
+
+        });
     });
 });
