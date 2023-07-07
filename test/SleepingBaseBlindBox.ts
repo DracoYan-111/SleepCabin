665c268bdbf72af3c946e2fdd35d8e9ed8c9b0dc
LongYan <2944972991@qq.com>
2023-07-07 14:26:56 +0800

    SBBB's Operation Authority Check

--- test/SleepingBaseBlindBox.ts
+++ test/SleepingBaseBlindBox.ts
@@ -1,9 +1,8 @@
 import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
-import { execSync } from 'child_process';
+import {execSync} from 'child_process';
 import {expect} from "chai";
 import {ethers} from "hardhat";
 import fs from 'fs';
-
 
 
 describe("SleepingBase", function () {
@@ -56,7 +55,7 @@ describe("SleepingBase", function () {
     function runGenerateMerkleRoot(): void {
         try {
             const command = 'ts-node scripts/generate-merkle-root.ts --input scripts/complex_example.json';
-            const output = execSync(command, { encoding: 'utf8' });
+            const output = execSync(command, {encoding: 'utf8'});
 
             console.log(output);
         } catch (error) {
@@ -68,10 +67,9 @@ describe("SleepingBase", function () {
     function readGenerateMerkleData(): any {
         try {
             const filePath = 'otherFiles/generateMerkle.json';
-            const fileData = fs.readFileSync(filePath, { encoding: 'utf8' });
-            const parsedData = JSON.parse(fileData);
+            const fileData = fs.readFileSync(filePath, {encoding: 'utf8'});
 
-            return parsedData;
+            return JSON.parse(fileData);
         } catch (error) {
             console.error('Error reading generateMerkle.json:', error);
             return null;
@@ -92,141 +90,94 @@ describe("SleepingBase", function () {
 
         const SleepingBaseBlindBox = await ethers.getContractFactory("contracts/SleepingBaseBlindBox.sol:SleepingBaseBlindBox");
 
-        runGenerateMerkleRoot();
+        //runGenerateMerkleRoot();
         const {sleepingBase} = await loadFixture(deploySleepingBase);
         let time = Math.floor(Date.now() / 1000)
         let openAndSalesTime = timeGeneration([(time + 1000).toString(), (time + 2000).toString()]);
         let tokenUri = "{\"image\": \"ipfs://QmdrJ3qoFpAw6s6cxh3JNXyrURo3UopF6p5B2ma3ZB8kTc/FCB_MASTERPIECE2_STATIC.png\"}"
+        let generateMerkleData = readGenerateMerkleData();
 
 
         const sleepingBaseBlindBox = await SleepingBaseBlindBox.deploy(
             sleepingBase.address,
             openAndSalesTime,
-            tokenUri
+            tokenUri,
+            generateMerkleData.merkleRoot,
+            999
         );
 
-        return {sleepingBaseBlindBox, owner, otherAccount};
+        return {sleepingBaseBlindBox, owner, otherAccount, sleepingBase, openAndSalesTime};
     }
 
-     describe("检查类", function () {
+    describe("检查类", function () {
         it("应该拥有正确的合约最高权限", async function () {
-            let ss = readGenerateMerkleData();
-            console.log(ss.merkleRoot)
+            const {sleepingBaseBlindBox, owner} = await loadFixture(deploySleepingBaseBlindBox);
 
+            expect(await sleepingBaseBlindBox.owner()).eq(owner.address);
         });
-    //
-    //     it("应该拥有正确的合约mint权限", async function () {
-    //         const {sleepingBase, owner} = await loadFixture(deploySleepingBase);
-    //         let MINTER_ROLE = await sleepingBase.MINTER_ROLE()
-    //         expect(await sleepingBase.hasRole(MINTER_ROLE, owner.address)).to.be.true;
-    //     });
-     });
 
-    // describe("操作类", function () {
-    //     describe("权限", function () {
-    //         it("因该可以增加他人权限,他人相反", async function () {
-    //             const {sleepingBase, otherAccount} = await loadFixture(deploySleepingBase);
-    //
-    //             let MINTER_ROLE = await sleepingBase.MINTER_ROLE()
-    //
-    //             await expect(sleepingBase.grantRole(MINTER_ROLE, otherAccount.address)).not.to.be.reverted
-    //             await expect(sleepingBase.connect(otherAccount).grantRole(MINTER_ROLE, otherAccount.address)).to.be.reverted;
-    //
-    //         });
-    //
-    //         it("因该可以暂停开启,他人相反", async function () {
-    //             const {sleepingBase, otherAccount} = await loadFixture(deploySleepingBase);
-    //
-    //             await expect(sleepingBase.pause()).not.to.be.reverted;
-    //             await expect(sleepingBase.unpause()).not.to.be.reverted;
-    //
-    //             await expect(sleepingBase.connect(otherAccount).pause()).to.be.reverted;
-    //             await expect(sleepingBase.connect(otherAccount).unpause()).to.be.reverted;
-    //
-    //         });
-    //
-    //         it("因该可以放弃权限", async function () {
-    //             const {sleepingBase, owner} = await loadFixture(deploySleepingBase);
-    //
-    //             let DEFAULT_ADMIN_ROLE = await sleepingBase.DEFAULT_ADMIN_ROLE()
-    //             let MINTER_ROLE = await sleepingBase.MINTER_ROLE()
-    //
-    //             await expect(sleepingBase.renounceRole(DEFAULT_ADMIN_ROLE, owner.address)).not.to.be.reverted;
-    //             await expect(sleepingBase.renounceRole(MINTER_ROLE, owner.address)).not.to.be.reverted;
-    //
-    //         });
-    //
-    //         it("因该可以撤销他人权限,他人相反", async function () {
-    //             const {sleepingBase, owner, otherAccount} = await loadFixture(deploySleepingBase);
-    //
-    //             let MINTER_ROLE = await sleepingBase.MINTER_ROLE()
-    //             await sleepingBase.grantRole(MINTER_ROLE, otherAccount.address)
-    //
-    //             await expect(sleepingBase.revokeRole(MINTER_ROLE, otherAccount.address)).not.to.be.reverted;
-    //             await expect(sleepingBase.connect(otherAccount).revokeRole(MINTER_ROLE, owner.address)).to.be.reverted;
-    //
-    //         });
-    //
-    //
-    //         it("因该可以进行增发操作,他人相反", async function () {
-    //             const {sleepingBase, owner, otherAccount} = await loadFixture(deploySleepingBase);
-    //
-    //             let combinedData = tokenIdGeneration()
-    //
-    //             await expect(sleepingBase.safeMint(owner.address, [combinedData], ["test"])).not.to.be.reverted;
-    //             await expect(sleepingBase.connect(otherAccount).safeMint(owner.address, [combinedData], ["test"])).to.be.reverted;
-    //
-    //         });
-    //
-    //         it("因该可以进行销毁操作,他人相反", async function () {
-    //             const {sleepingBase, owner, otherAccount} = await loadFixture(deploySleepingBase);
-    //
-    //             let combinedData = tokenIdGeneration()
-    //             let [tokenId] = tokenIdAnalysis(combinedData);
-    //
-    //             await sleepingBase.safeMint(owner.address, [combinedData], ["test"])
-    //
-    //             await expect(sleepingBase.safeBurn(tokenId)).not.to.be.reverted;
-    //             await expect(sleepingBase.connect(otherAccount).safeBurn(tokenId)).to.be.reverted;
-    //
-    //         });
-    //
-    //         it("因该可以进行URI修改,他人相反", async function () {
-    //             const {sleepingBase, owner, otherAccount} = await loadFixture(deploySleepingBase);
-    //
-    //             let combinedData = tokenIdGeneration()
-    //             let [tokenId] = tokenIdAnalysis(combinedData);
-    //
-    //             await sleepingBase.safeMint(owner.address, [combinedData], ["test"])
-    //
-    //             await expect(sleepingBase.steTokenUri(tokenId, "test1")).not.to.be.reverted;
-    //             await expect(sleepingBase.connect(otherAccount).steTokenUri(tokenId, "test1")).to.be.reverted;
-    //         });
-    //     });
-    //
-    //
-    //     describe("转账", function () {
-    //         it("NFT拥有者应该可以转账", async function () {
-    //             const {sleepingBase, owner, otherAccount} = await loadFixture(deploySleepingBase);
-    //
-    //             let combinedData = tokenIdGeneration();
-    //             await sleepingBase.safeMint(owner.address, [combinedData], ["test"]);
-    //
-    //             let [tokenId] = tokenIdAnalysis(combinedData);
-    //             await sleepingBase.setApprovalForAll(otherAccount.address, true);
-    //
-    //             await expect(sleepingBase.transferFrom(owner.address, otherAccount.address, tokenId)).not.to.be.reverted;
-    //         });
-    //
-    //         it("NFT未拥有者不应该可以转账", async function () {
-    //             const {sleepingBase, owner, otherAccount} = await loadFixture(deploySleepingBase);
-    //
-    //             let combinedData = tokenIdGeneration();
-    //             let [tokenId] = tokenIdAnalysis(combinedData);
-    //             await sleepingBase.connect(otherAccount).setApprovalForAll(owner.address, true);
-    //
-    //             await expect(sleepingBase.connect(otherAccount).transferFrom(otherAccount.address, owner.address, tokenId)).to.be.reverted;
-    //         });
-    //     });
-    // });
+        it("应该拥有正确的merkleRoot", async function () {
+            const {sleepingBaseBlindBox} = await loadFixture(deploySleepingBaseBlindBox);
+
+            let generateMerkleData = readGenerateMerkleData();
+
+            expect(await sleepingBaseBlindBox.merkleRoot()).eq(generateMerkleData.merkleRoot);
+        });
+
+        it("应该拥有正确的sleepingBase合约地址", async function () {
+            const {sleepingBaseBlindBox, sleepingBase} = await loadFixture(deploySleepingBaseBlindBox);
+
+            expect(await sleepingBaseBlindBox.sleepingBase()).eq(sleepingBase.address);
+        });
+    });
+
+    describe("操作类", function () {
+        describe("权限", function () {
+            it("应该可以暂停开启合约,他人相反", async function () {
+                const {sleepingBaseBlindBox, otherAccount} = await loadFixture(deploySleepingBaseBlindBox);
+
+                await expect(sleepingBaseBlindBox.connect(otherAccount).pause()).to.be.reverted;
+                await expect(sleepingBaseBlindBox.pause()).not.to.be.reverted
+
+                await expect(sleepingBaseBlindBox.connect(otherAccount).unpause()).to.be.reverted;
+                await expect(sleepingBaseBlindBox.unpause()).not.to.be.reverted
+            });
+
+            it("应该可以修改时间,他人相反", async function () {
+                const {sleepingBaseBlindBox, otherAccount} = await loadFixture(deploySleepingBaseBlindBox);
+
+                await expect(sleepingBaseBlindBox.connect(otherAccount).setOpenAndSalesTime(timeGeneration(["1688710482", "1688710482"]))).to.be.reverted;
+                await expect(sleepingBaseBlindBox.setOpenAndSalesTime(timeGeneration(["1688710482", "1688710482"]))).not.to.be.reverted
+            });
+
+            it("应该可以放弃权限和转移权限,他人相反", async function () {
+                const {sleepingBaseBlindBox, owner, otherAccount} = await loadFixture(deploySleepingBaseBlindBox);
+                await expect(sleepingBaseBlindBox.transferOwnership(owner.address)).not.to.be.reverted;
+                await expect(sleepingBaseBlindBox.connect(otherAccount).transferOwnership(owner.address)).to.be.reverted;
+
+                await expect(sleepingBaseBlindBox.renounceOwnership()).not.to.be.reverted;
+                await expect(sleepingBaseBlindBox.connect(otherAccount).renounceOwnership()).to.be.reverted;
+            });
+
+            it("应该可以修改merkleRoot,他人相反", async function () {
+                const {sleepingBaseBlindBox, otherAccount} = await loadFixture(deploySleepingBaseBlindBox);
+
+                let generateMerkleData = readGenerateMerkleData();
+
+                await expect(sleepingBaseBlindBox.setMerkleRoot(generateMerkleData.merkleRoot)).not.to.be.reverted;
+                await expect(sleepingBaseBlindBox.connect(otherAccount).setMerkleRoot(generateMerkleData.merkleRoot)).to.be.reverted;
+            });
+
+            it("因该可以进行增发操作,他人相反", async function () {
+                const {sleepingBaseBlindBox, owner, otherAccount} = await loadFixture(deploySleepingBaseBlindBox);
+
+                await expect(sleepingBaseBlindBox.mint(owner.address, 99)).not.to.be.reverted;
+                await expect(sleepingBaseBlindBox.connect(otherAccount).mint(otherAccount.address, 99)).to.be.reverted;
+
+            });
+        });
+
+        // describe("用户操作", function () {
+        // });
+    });
 });
