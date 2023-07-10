2ed076131a5af5418247277234c00898e2948383
LongYan <2944972991@qq.com>
2023-07-10 18:37:01 +0800

    update scripts/deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,13 @@
 import {ethers} from "hardhat";
+import {
+    getApprovalDigest,
+    getDomainSeparator,
+    PERMIT_TYPEHASH,
+    runGenerateMerkleRoot,
+    tokenIdGeneration,
+    timeGeneration
+} from "../test/utils/utils"
+import {ecsign} from 'ethereumjs-util'
 
 async function main() {
     // // 创建以太坊签名对象
@@ -14,36 +23,60 @@ async function main() {
     // console.log('r:', r);
     // console.log('s:', s);
 
-    const permitTypeHash = ''; // 你需要替换为正确的 typehash
-    const nonce = 0
-    const message = {
-        owner: "0x001d1dC40383a700aC94B364EA82d0AecBe51127",
-        spender: "0xDDC22a2e784dbe167FAa17a63F4dB4B0b81252Ed",
-        value: 10000000000,
-        deadline: 1688364775,
-        nonce: 0,
-    };
-    const domain = {
-        name: 'MyToken',
-        version: '1',
-        chainId: 80001, // 你需要替换为正确的链ID
-        verifyingContract: '0x62e015e500cf58c870dC27483357A878FFbC7BfF', // 你需要替换为正确的合约地址
-    };
-    const types = {
-        Permit: [
-            {name: 'owner', type: 'address'},
-            {name: 'spender', type: 'address'},
-            {name: 'value', type: 'uint256'},
-            {name: 'deadline', type: 'uint256'},
-            {name: 'nonce', type: 'uint256'},
-        ],
-    };
-    const provider = ethers.getDefaultProvider("https://rpc.ankr.com/polygon_mumbai");
+    // const permitTypeHash = ''; // 你需要替换为正确的 typehash
+    // const nonce = 0
+    // const message = {
+    //     owner: "0x001d1dC40383a700aC94B364EA82d0AecBe51127",
+    //     spender: "0xDDC22a2e784dbe167FAa17a63F4dB4B0b81252Ed",
+    //     value: 10000000000,
+    //     deadline: 1688364775,
+    //     nonce: 0,
+    // };
+    // const domain = {
+    //     name: 'MyToken',
+    //     version: '1',
+    //     chainId: 80001, // 你需要替换为正确的链ID
+    //     verifyingContract: '0x62e015e500cf58c870dC27483357A878FFbC7BfF', // 你需要替换为正确的合约地址
+    // };
+    // const types = {
+    //     Permit: [
+    //         {name: 'owner', type: 'address'},
+    //         {name: 'spender', type: 'address'},
+    //         {name: 'value', type: 'uint256'},
+    //         {name: 'deadline', type: 'uint256'},
+    //         {name: 'nonce', type: 'uint256'},
+    //     ],
+    // };
+    // const provider = ethers.getDefaultProvider("https://rpc.ankr.com/polygon_mumbai");
+    //
+    // const signer = new ethers.Wallet("", provider);
+    // const signature = await signer._signTypedData(domain, types, message);
+    // const {v, r, s} = ethers.utils.splitSignature(signature);
+    // console.log(v, r, s)
 
-    const signer = new ethers.Wallet("", provider);
-    const signature = await signer._signTypedData(domain, types, message);
-    const {v, r, s} = ethers.utils.splitSignature(signature);
-    console.log(v, r, s)
+    //runGenerateMerkleRoot();
+    console.log(PERMIT_TYPEHASH)
+    console.log(getDomainSeparator("SleepingBaseBlindBox", "0x9D7f74d0C41E726EC95884E0e97Fa6129e3b5E99"))
+    // console.log(tokenIdGeneration(["2", "5", "16", "25", "23", "67"]));
+    console.log(timeGeneration(["1688975325", "1688975325"]));
+
+    const digest = await getApprovalDigest(
+        "SleepingBaseBlindBox",
+        "0x5FD6eB55D12E759a21C09eF703fe0CBa1DC9d88D",
+        {
+            value: 1,
+            tokenIds: ["12554203470773361529372990682287675750210981929426352078871"],
+            uris: ["test"]
+        },
+        2,
+        1698984438
+    )
+    console.log(digest)
+    let privateKey = "0xbd1d2b53a2fafe949523b2a3bc70b82bf6005a62c29a70de81acaaf08abe3d0f";
+    const {v, r, s} = ecsign(Buffer.from(digest.slice(2), 'hex'), Buffer.from(privateKey.slice(2), 'hex'))
+    console.log(v)
+    console.log("0x" + r.toString("hex"))
+    console.log("0x" + s.toString("hex"))
 }
 
 
