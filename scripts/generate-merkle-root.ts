64edb4e8fd667b6c01df0acb5f67fc830dff7adf
LongYan <2944972991@qq.com>
2023-07-12 15:54:47 +0800

    fix package.json

--- scripts/generate-merkle-root.ts
+++ scripts/generate-merkle-root.ts
@@ -6,18 +6,18 @@ program
     .version('0.0.0')
     .requiredOption(
         '-i, --input <path>',
-        'input JSON file location containing a map of account addresses to string balances'
+        '输入 JSON 文件位置，其中包含账户地址到字符串余额的映射'
     );
 
 program.parse(process.argv);
 
 const json = JSON.parse(fs.readFileSync(program.input, { encoding: 'utf8' }));
 
-if (typeof json !== 'object') throw new Error('Invalid JSON');
+if (typeof json !== 'object') throw new Error('无效的 JSON');
 
 const parsedResult = parseBalanceMap(json);
 const outputFile = 'otherFiles/generateMerkle.json';
 
 fs.writeFileSync(outputFile, JSON.stringify(parsedResult, null, 2));
 
-console.log(`Parsed balance map saved to ${outputFile}`);
+console.log(`解析后的数据已保存到 ${outputFile}`);
