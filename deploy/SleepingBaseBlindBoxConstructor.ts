31385f05db6902deb83894423c0e41cadd297175
LongYan <2944972991@qq.com>
2023-07-13 14:47:55 +0800

    update deploy/SleepingBaseBlindBoxConstructor.ts

--- deploy/SleepingBaseBlindBoxConstructor.ts
+++ deploy/SleepingBaseBlindBoxConstructor.ts
@@ -3,15 +3,18 @@ import {
     readGenerateData,
 } from "../utils/utils";
 
-let deployments = readGenerateData('deployments/localhost_deployment.json');
 let generateMerkleData = readGenerateData('otherFiles/generateMerkle.json');
 
+//SleepingBase sleepingBase_
+//uint256 openAndSalesTime_
+//string memory tokenUri_
+//bytes32 merkleRoot_
+//address verifier_
 const data = [
-    deployments.address,                                  //SleepingBase sleepingBase_
-    timeGeneration(['1689154377', '1689154377']),   //uint256 openAndSalesTime_
-    "tokenUri",                                           //string memory tokenUri_
-    generateMerkleData.merkleRoot,                        //bytes32 merkleRoot_
-    deployments.receipt["from"],                          //address verifier_
-
+    "0x5ab8C46e98D6f86496C0b415110ABB0Cd734F6Af",
+    timeGeneration(['1689154377', '1689154377']),
+    "tokenUri",
+    generateMerkleData.merkleRoot,
+    "0x5ab8C46e98D6f86496C0b415110ABB0Cd734F6Af",
 ];
 export {data};
