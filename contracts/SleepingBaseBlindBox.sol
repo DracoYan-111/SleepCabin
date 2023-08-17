3912973ec2dd980938851a1a60f48963eed2ad63
LongYan <2944972991@qq.com>
2023-08-17 15:32:02 +0800

    add setOpenAndSalesTime function

--- contracts/SleepingBaseBlindBox.sol
+++ contracts/SleepingBaseBlindBox.sol
@@ -24,15 +24,15 @@ contract SleepingBaseBlindBox is ERC721, ERC721Enumerable, Pausable, Ownable, Bl
 
     /* ========== EVENTS ========== */
 
-    event UserMint(address userAddress, uint256[] tokenIds, bool isFreeMint);
+    event UserMint(uint256 mintTime, address userAddress, uint256 length);
     event OpenBox(uint256 openTime, address userAddress, uint256 amount);
 
     /* ========== STATE VARIABLES ========== */
 
     // This is a compressed value of the turn-on time and transition time
-    string private  _onlyTokenUri;
+    string private _onlyTokenUri;
     uint256 private _openAndSalesTime;
-    address public accountsReceivable;
+    address public immutable accountsReceivable;
     SleepingBase public immutable sleepingBase;
 
     /* ========== CONSTRUCTOR ========== */
@@ -41,10 +41,12 @@ contract SleepingBaseBlindBox is ERC721, ERC721Enumerable, Pausable, Ownable, Bl
         SleepingBase sleepingBase_,
         uint256 openAndSalesTime_,
         string memory tokenUri_,
+        address accountsReceivable_,
         address verifier_
     )
     ERC721("SleepingBaseBlindBox", "")
     BlindBoxPermit(verifier_) {
+        accountsReceivable = accountsReceivable_;
         _openAndSalesTime = openAndSalesTime_;
         sleepingBase = sleepingBase_;
         _onlyTokenUri = tokenUri_;
@@ -63,19 +65,31 @@ contract SleepingBaseBlindBox is ERC721, ERC721Enumerable, Pausable, Ownable, Bl
         _safeMint(to, tokenId);
     }
 
+    /**
+    * @dev Set open and sales time
+    * @notice Only used by Owner
+    * @param openAndSalesTime_ Open time and sales time
+    */
+    function setOpenAndSalesTime(uint256 openAndSalesTime_)
+    public
+    onlyOwner {
+        _openAndSalesTime = openAndSalesTime_;
+    }
+
     /* ========== MUTATIVE FUNCTIONS ========== */
 
     /**
     * @dev user mint blind box permit
     * @notice It cannot be called before the time is up,
     *         and the interface can be locked
-    * @param packed  uint256 paymentAmount,bool isFreeMint
+    * @param paymentAmount Payment amount
     * @param tokenIds Array of token ids to be issued
     * @param deadline Maximum effective time
     */
     function userMintPermit(
-        uint256 packed,
-        uint256 [] calldata tokenIds,
+        address userAddress,
+        uint256 paymentAmount,
+        uint256[] calldata tokenIds,
         uint deadline,
         uint8 v,
         bytes32 r,
@@ -86,19 +100,19 @@ contract SleepingBaseBlindBox is ERC721, ERC721Enumerable, Pausable, Ownable, Bl
     payable
     whenNotPaused {
 
-        super.userMintPermit(packed, tokenIds, deadline, v, r, s);
+        super.userMintPermit(userAddress, paymentAmount, tokenIds, deadline, v, r, s);
 
-        (uint256 paymentAmount,uint128 isFreeMint) = _unpackedValue(packed);
-
-        if (isFreeMint == 0)
-            paymentAmount != msg.value ?
+        uint256 tokenIdsLength = tokenIds.length;
+        if (paymentAmount > 0)
+            paymentAmount * tokenIdsLength != msg.value ?
             revert("unsatisfiedPayment") : safeTransferETH(accountsReceivable, paymentAmount);
 
-
-        for (uint256 i; i < tokenIds.length;) {
-            _safeMint(_msgSender(), tokenIds[i]);
+        for (uint256 i; i < tokenIdsLength;) {
+            _safeMint(userAddress, tokenIds[i]);
         unchecked{++i;}
         }
+
+        emit UserMint(block.timestamp, userAddress, tokenIdsLength);
     }
 
     /**
@@ -112,6 +126,7 @@ contract SleepingBaseBlindBox is ERC721, ERC721Enumerable, Pausable, Ownable, Bl
     */
     function openBoxPermit(
         uint256 amount,
+        address userAddress,
         uint256[] calldata tokenIds,
         string[] calldata uris,
         uint deadline,
@@ -125,16 +140,22 @@ contract SleepingBaseBlindBox is ERC721, ERC721Enumerable, Pausable, Ownable, Bl
         if (block.timestamp < openingTime)
             revert NotYetOpeningTime(block.timestamp);
 
-        super.openBoxPermit(amount, tokenIds, uris, deadline, v, r, s);
+        super.openBoxPermit(amount, userAddress, tokenIds, uris, deadline, v, r, s);
         for (uint256 i; i < amount;) {
             // Destroy the blind box first
-            super._burn(tokenOfOwnerByIndex(_msgSender(), balanceOf(_msgSender()) - 1));
+            super._burn(tokenOfOwnerByIndex(userAddress, balanceOf(_msgSender()) - 1));
         unchecked{++i;}
         }
 
-        sleepingBase.safeMint(_msgSender(), tokenIds, uris);
+        sleepingBase.safeMint(userAddress, tokenIds, uris);
 
-        emit OpenBox(block.timestamp, _msgSender(), amount);
+        emit OpenBox(block.timestamp, userAddress, amount);
+    }
+
+    function safeTransferETH(address to, uint value) internal {
+        (bool success,) = to.call{value : value}(new bytes(0));
+        if (!success)
+            revert("ETH_TRANSFER_FAILED");
     }
 
     /* ========== VIEWS FUNCTION ========== */
@@ -149,15 +170,13 @@ contract SleepingBaseBlindBox is ERC721, ERC721Enumerable, Pausable, Ownable, Bl
     }
     }
 
-    function safeTransferETH(address to, uint value) internal {
-        (bool success,) = to.call{value : value}(new bytes(0));
-        if (!success)
-            revert("ETH_TRANSFER_FAILED");
-    }
-
     /* ========== OVERRIDE FUNCTIONS ========== */
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize)
     internal
     whenNotPaused
     override(ERC721, ERC721Enumerable)
