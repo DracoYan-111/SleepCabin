a8c7ccae63f85e8e1dfd9231a3378a1b4356cac7
LongYan <2944972991@qq.com>
2023-08-17 15:34:04 +0800

    update scripts/deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,7 @@
-import {
-    getApprovalDigest,
-    getDomainSeparator,
-    PERMIT_TYPEHASH,
-    timeGeneration
-} from "../utils/utils"
-import {ecsign} from 'ethereumjs-util'
 import {ethers} from "hardhat";
 
 async function main() {
+    // 给其他用户转账
     const amountEther = 100; // 转账金额（以太）
     const [owner, otherAccount] = await ethers.getSigners();
     let tx = await owner.sendTransaction({
@@ -17,31 +11,6 @@ async function main() {
     console.log('Transaction hash:', tx.hash);
     await tx.wait(); // 等待交易完成
     console.log('Transaction confirmed in block:', tx.blockNumber);
-
-
-    //runGenerateMerkleRoot();
-    console.log(PERMIT_TYPEHASH)
-    console.log(getDomainSeparator("SleepingBaseBlindBox", "0x9D7f74d0C41E726EC95884E0e97Fa6129e3b5E99"))
-    // console.log(tokenIdGeneration(["2", "5", "16", "25", "23", "67"]));
-    console.log(timeGeneration(["1688975325", "1688975325"]));
-
-    const digest = await getApprovalDigest(
-        "SleepingBaseBlindBox",
-        "0x9a2E12340354d2532b4247da3704D2A5d73Bd189",
-        {
-            value: 1,
-            tokenIds: ["12554203470773361529372990682287675750210981929426352078871"],
-            uris: ["test"]
-        },
-        0,
-        1789067245
-    )
-    console.log(digest)
-    let privateKey = "0xbd1d2b53a2fafe949523b2a3bc70b82bf6005a62c29a70de81acaaf08abe3d0f";
-    const {v, r, s} = ecsign(Buffer.from(digest.slice(2), 'hex'), Buffer.from(privateKey.slice(2), 'hex'))
-    console.log(v)
-    console.log("0x" + r.toString("hex"))
-    console.log("0x" + s.toString("hex"))
 }
 
 
